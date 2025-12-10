2ea1e0a9c57d77ba411f24a5ae554e1dc75eccc2
Carlos Daniel Morera Pinilla <117225812+Cdmpc@users.noreply.github.com>
2025-12-09 17:05:08 -0700

    user-gen.py exploit created, merged to main. (#1)
    * Exploit should create Secrets directory if it does not exist.
    
    * moved user generation script to Client, updated README, to reflect current stage.
    
    * user-gen scripts now stores the SHA256 hash of the secret message to the .txt files in Secrets directory.
    
    * updated README to reflect hash in files.
    
    * passwords for each user in Python user-gen script are now psuedorandom and 20 characters long.
    
    * Added hash file repo (gitignored, but will show up when you run user-gen.py). Updated README

---
+++ Client/user-gen.py
@@ -0,0 +1,154 @@
+# ============================================ [START OF EXPLOIT SCRIPT] ============================================ #
+import os;
+import sys;
+import pexpect as pex;
+import pexpect.popen_spawn as pop;
+import signal;
+import pathlib as plib;
+import hashlib;
+import random;
+import string;
+
+DEBUG = os.environ.get("DEBUG", "0").lower() in ["1", "y", "yes", "true", "on", "color"];
+
+# NOTE: This exploit script is meant to be run from the REPOSITORY DIRECTORY.
+# i.e, the directory that is created when you CLONE this repository.
+# ============================================= [HELPER FUNCTIONS] ============================================== #
+def rm_all_files(dir_path):
+    secrets_dir = dir_path;
+    files = os.listdir(secrets_dir);
+
+    if (len(files) > 0):
+        for name in files:
+            file = secrets_dir + name;
+            if os.path.isfile(file):
+                os.remove(file);
+            else:
+                continue;
+    else:
+        print("Directory is already empty, no files to remove...");
+
+
+def create_N_notes(N, IP_ADDR, PORT):
+    # Create the N users
+    for i in range(1, N + 1):
+        # Open up the client as well, on the same port.
+        cli_process = pop.PopenSpawn(f"stdbuf -o0 ./Client/client {IP_ADDR} {PORT}", timeout=5);
+        cli_process.delaybeforesend = None;
+
+        if DEBUG:
+            cli_process.logfile = sys.stdout.buffer;
+        # Create the user metadata.
+        cli_process.expect_exact("Do you already have and account (Y/N) ? : ");
+        cli_process.sendline("N");
+
+        # Send in sample usernames with the same format : user_i
+        cli_process.expect_exact("Please enter your username: ");
+        cli_process.sendline(f"user_{i}");
+
+        # Send length-20 passwords of psuedorandom alphanum passwords: [a-zA-Z0-9]
+        cli_process.expect_exact("Please enter your password: ");
+        pseudo_rand_pw = "".join(random.choices(string.ascii_letters + string.digits, k=20));
+        cli_process.sendline(pseudo_rand_pw);
+
+        # NOTE: The temp.txt file is created automatically from the C++ program, 
+        # once you enter a username and password.
+        cli_process.expect_exact("(1, 2, 3) ? : ");
+
+        # NOTE: Make sure to write to the created temp.txt file BEFORE entering "1" input.
+        with open("./temp.txt", "w") as fp:
+            # Write a psuedorandom N-length a-zA-Z0-9 string as the secret message.
+            secret_message = f"[{i}] ===> {"".join(random.choices(string.ascii_letters + string.digits, k=30))}";
+            fp.write(secret_message);
+
+        # Upload the file, which can be found at Secrets directory.
+        cli_process.sendline("1");
+
+        # Close the process, as the program will terminate once we enter and write the note and 
+        # "1" input.
+        cli_process.expect(pex.EOF, timeout=5);
+        cli_process.kill(signal.SIGTERM);
+
+    print(f"{N} users created, returning from function...");
+
+# CREATES A SINGLE FILE THAT CONTAINS THE SHA256 HASHES OF EACH SECRET MESSAGE.
+def create_hashes():
+    # Get all the file names from Secrets
+    secrets_dir = "./Secrets";
+    files = os.listdir(secrets_dir);
+
+    # Create HASH repo file
+    with open("./Exploit/hash-repo.txt", "w") as fp_repo:
+        for file in files:
+            # Robust DIR + FILE path joining.
+            file_path = os.path.join(secrets_dir, file);
+            # Skip non-files
+            if not (os.path.isfile(file_path)):
+                continue;
+
+            # SHA256 Hasher object and buffer to read secret.
+            sha256_hasher = hashlib.sha256();
+
+            # Open the Secrets files
+            with open(file_path, "rb") as fp_secret:
+                # Read the Secret contents
+                secret = fp_secret.read();
+
+                # Hash the secret, and write it to the REPO file.
+                sha256_hasher.update(secret);
+                fp_repo.write(f"HASH OF {file} ==> {sha256_hasher.hexdigest()}\n");
+
+    # Confirm hashes were complete.
+    print(f"HASHING COMPLETE CHECK ./Exploit/hash-repo.txt file");
+
+
+# ============================================= [MAIN DEFINITION] ============================================== #
+def main(IP_ADDRESS, PORT_NO):
+    try:
+        # Delete the temp file if it exists before running the exploit script.
+        # Also delete the secret text files in the Secrets directory.
+        plib.Path("./temp.txt").unlink(missing_ok=True);
+
+        # Create the "Secrets" directory, if it does not exist.
+        plib.Path("./Secrets").mkdir(exist_ok=True);
+        valid_input = False;
+
+        # User creation loop.
+        while (not valid_input):
+            N_users = input("How many users do you want to create (MAXIMUM 100): ").strip();
+            if (N_users.isdigit() and (int(N_users) <= 100 and int(N_users) > 0)):
+                N_users = int(N_users);
+                valid_input = True;
+                # Let's create N users
+                create_N_notes(N=N_users, IP_ADDR=IP_ADDRESS, PORT=PORT_NO);
+                create_hashes();
+                print("create_N_notes() succeeded, please check ./Secrets directory.");
+            
+            # If the user did not put in anything for created users, just use the default of 50.
+            elif (N_users == ""):
+                print("No input detected, using default value of 50 users...");
+                valid_input = True;
+                create_N_notes(N=50, IP_ADDR=IP_ADDRESS, PORT=PORT_NO);
+                create_hashes();
+                print("create_N_notes() succeeded, please check ./Secrets directory.");
+            
+            else:
+                print("INVALID INPUT, PLEASE ENTER A NUMBER <= 100\n");
+    
+        # Clear the temp file once it's done.
+        plib.Path("./temp.txt").unlink(missing_ok=True);
+
+    except Exception:
+        print("create_N_notes() failed");
+
+# ============================================= [MAIN CALL AND STARTING POINT] ============================================== #
+if __name__ == "__main__":
+    try:
+        print("============================== [EXPLOIT START] ===================================\n");
+        main(sys.argv[1], sys.argv[2]);
+        print("\n============================== [EXPLOIT TERMINATED] ==============================");
+    except Exception:
+        print("\nCAUGHT ERROR: main() function failed, did you try: ");
+        print("argv[1] == IP ADDRESS (127.0.0.1 for example)");
+        print("argv[2] == PORT NUMBER (Ex: 5400)");
+        print("Usage: python3 ./Client/user-gen.py argv[1] argv[2]");
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CURRENT STAGE OF EXPLOIT (INCOMPLETE)
+# CURRENT STAGE OF EXPLOIT (INCOMPLETE - USER CREATION COMPLETE, NO EXPLOIT YET)
 
 Authored by: Shawn Hanlon & Carlos Morera Pinilla
 
@@ -26,17 +26,29 @@ g++ -std=c++20 -Wall -o ./Server/server ./Server/server.cpp
 g++ -std=c++20 -Wall -o ./Client/client ./Client/client.cpp
 ```
 
-### 3. Run the Python Exploit Script
+### 2.5: Start the SERVER PROGRAM FIRST (Example PORT 7000)
+```
+./Server/server 7000
+```
+
+If you see this: 
+```
+LOG::port : 7000
+```
+The server is connected and ready to go.
+
+### 3. Run the Python Exploit Script, using example 127.0.0.1 and port 7000
+#### NOTE: Use the SAME port number for the Client that you started the Server with.
 ```bash
-python3 ./Exploit/heap-exploit.py ./Server/server ./Client/client
+python3 ./Client/user-gen.py 127.0.0.1 7000
 ```
 
 If you do not provide command-line arguements to the Python script, this prompt will appear.
 ```bash
-argv[1] == <path_to_server_executable>
-argv[2] == <path_to_client_executable>
-Usage: python3 ./Exploit/heap-exploit.py argv[1] argv[2]
-Example: python3 ./Exploit/heap-exploit.py ./Server/server ./Client/client
+CAUGHT ERROR: main() function failed, did you try:
+"argv[1] == IP ADDRESS (127.0.0.1 for example)"
+"argv[2] == PORT NUMBER (Ex: 5400)"
+"Usage: python3 ./Exploit/heap-exploit.py argv[1] argv[2]"
 ```
 ### 4. Follow the prompt:
 ```
@@ -48,10 +60,24 @@ create_N_notes() succeeded, please check ./Secrets directory.
 
 ============================== [EXPLOIT TERMINATED] ==============================
 ```
+The passwords, will be psuedo-random 20 character strings of range [0-9A-Za-z]
 
 ### 5. The secret files will be located in a created directory called ./Secrets, where your current working directory is the repository's directory.
 
 The secret files will have the name format "userN.txt" and contain the following format:
 ```
-Hi, this is USER_N, and this file is my secret!
+[N] ==> {30 psuedorandom strings of range [0-9A-Za-z]}
 ```
+
+## NOTE: Limitation, the Client can only run once, subsequent runs of the Client/automation script will cause it to fail. To run it successfuly again, you must manually close and restart the Server, then run the Client again once the Server restarts.
+
+### 6. In the Exploit directory, a file called hash-repo.txt will be created.
+It will have the following format: 
+```
+HASH OF user1.txt ==> [SHA256 HASH OF USER1]
+
+HASH OF user2.txt ==> [SHA256 HASH OF USER2]
+
+HASH OF user3.txt ==> [SHA256 HASH OF USER3]
+```
+These hashes will be used to verify that we have indeed gotten the correct file when we run the exploit, and did not just hardcode it.
